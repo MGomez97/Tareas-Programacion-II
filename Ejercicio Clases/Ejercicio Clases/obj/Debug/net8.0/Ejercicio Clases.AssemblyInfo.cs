cf47bc3d8d5c0445d549e713219a44aa89d7a652
MGomez97 <manuelgomez19010@gmail.com>
2024-07-28 10:31:04 -0600

    Update Ejercicio Clases.AssemblyInfo.cs

--- Ejercicio Clases/Ejercicio Clases/obj/Debug/net8.0/Ejercicio Clases.AssemblyInfo.cs
+++ Ejercicio Clases/Ejercicio Clases/obj/Debug/net8.0/Ejercicio Clases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio Clases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04a79942a96d7c0cf0e9037721d824eb33e9699")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio Clases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio Clases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
